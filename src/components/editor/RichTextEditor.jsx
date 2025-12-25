88d04b202ba713db21bc0bd79534bb38c4c998ba
PhyG0 <zebra9989zebra@gmail.com>
2025-12-26 03:33:57 +0530

    Enable YouTube video embedding in editor

--- src/components/editor/RichTextEditor.jsx
+++ src/components/editor/RichTextEditor.jsx
@@ -47,6 +47,32 @@ const RichTextEditor = ({ value, onChange, onImageUpload, placeholder = 'Write y
         };
     };
 
+    // Custom video handler
+    const videoHandler = () => {
+        const url = prompt('Enter video URL (YouTube, Vimeo):');
+        if (url) {
+            let finalUrl = url;
+
+            // Convert YouTube watch URL to embed URL
+            // Pattern: https://www.youtube.com/watch?v=VIDEO_ID -> https://www.youtube.com/embed/VIDEO_ID
+            if (url.includes('youtube.com/watch?v=')) {
+                finalUrl = url.replace('watch?v=', 'embed/');
+                // Remove any other query params
+                if (finalUrl.includes('&')) {
+                    finalUrl = finalUrl.split('&')[0];
+                }
+            }
+            // Pattern: https://youtu.be/VIDEO_ID -> https://www.youtube.com/embed/VIDEO_ID
+            else if (url.includes('youtu.be/')) {
+                finalUrl = url.replace('youtu.be/', 'youtube.com/embed/');
+            }
+
+            const quill = quillRef.current.getEditor();
+            const range = quill.getSelection(true);
+            quill.insertEmbed(range.index, 'video', finalUrl);
+        }
+    };
+
     const modules = useMemo(() => ({
         toolbar: {
             container: [
@@ -54,13 +80,14 @@ const RichTextEditor = ({ value, onChange, onImageUpload, placeholder = 'Write y
                 ['bold', 'italic', 'underline', 'strike'],
                 [{ 'list': 'ordered' }, { 'list': 'bullet' }],
                 [{ 'indent': '-1' }, { 'indent': '+1' }],
-                ['link', 'image'],
+                ['link', 'image', 'video'],
                 ['blockquote', 'code-block'],
                 [{ 'align': [] }],
                 ['clean']
             ],
             handlers: {
-                image: imageHandler
+                image: imageHandler,
+                video: videoHandler
             }
         }
     }), []);
@@ -69,7 +96,7 @@ const RichTextEditor = ({ value, onChange, onImageUpload, placeholder = 'Write y
         'header',
         'bold', 'italic', 'underline', 'strike',
         'list', 'bullet', 'indent',
-        'link', 'image',
+        'link', 'image', 'video',
         'blockquote', 'code-block',
         'align'
     ];
